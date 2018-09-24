0e09882292dc7f35ff96adac51a41d79daba2006
Anthony  Amsellem <anamsell@z4r7p3.le-101.fr>
2018-09-24 18:52:37 +0200

    issou

--- compiler/includes/compiler.h
+++ compiler/includes/compiler.h
@@ -3,10 +3,10 @@
 /*                                                              /             */
 /*   compiler.h                                       .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: bpisano <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*   By: anamsell <anamsell@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/09/24 17:57:34 by bpisano      #+#   ##    ##    #+#       */
-/*   Updated: 2018/09/24 17:48:50 by bpisano     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/09/24 18:52:26 by anamsell    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -25,5 +25,10 @@ int		*new_bin();
 int		add_to_bin(int **bin, int b);
 int		*str_to_bin(char *str);
 void	free_binary(int **bin);
+
+/*
+ ** READ_FILE
+*/
+char**  ReadFile(char *filename);
 
 #endif
