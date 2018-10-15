89ece7f18c9c27896c5aa44c029bb8ab6a7a2cb1
Anthony  Amsellem <anamsell@z4r8p1.le-101.fr>
2018-10-15 14:06:31 +0200

    free

--- compiler/sources/free_split.c
+++ compiler/sources/free_split.c
@@ -3,10 +3,10 @@
 /*                                                              /             */
 /*   free_split.c                                     .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: bpisano <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*   By: anamsell <anamsell@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/15 11:14:34 by bpisano      #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/05 13:11:51 by bpisano     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/15 14:06:18 by anamsell    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -21,4 +21,23 @@ void			free_split(char ***split)
 	while ((*split)[++i])
 		free((*split)[i]);
 	free(*split);
+}
+
+int				free_struct(t_pos data, t_op *op, t_lab **lab, int i)
+{
+	printf("ok\n");
+	free(op);
+	data.i = -1;
+	while (data.file[++data.i] && (data.j = -1))
+	{
+		while (data.file[data.i][++data.j])
+			free(data.file[data.i][data.j]);
+		free(data.file[data.i]);
+	}
+	free(data.file);
+	data.i = -1;
+	while (lab[++data.i])
+		free(lab[data.i]);
+	free(lab);
+	return (i);
 }
