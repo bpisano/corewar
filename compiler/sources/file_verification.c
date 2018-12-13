78ef972dd60d4ce23854e2ee94859a68da837800
bpisano <bpisano@z4r7p5.le-101.fr>
2018-12-13 20:20:28 +0100

    fix

--- compiler/sources/file_verification.c
+++ compiler/sources/file_verification.c
@@ -101,7 +101,7 @@ char		verify_syntax(char **line, int l)
 		return (ft_error("ERROR line %d : Separator at end of line\n~~~ %s ~~~\n",
 				l, *line) & 0);
 	if (!arguments_are_correct_formatted(*line + i))
-		return (ft_error("ERROR line %d : Argument are badly formated\n~~~%s~~~\n",
+		return (ft_error("ERROR line %d : Argument are badly formated\n~~~ %s ~~~\n",
 				l, *line) & 0);
 	str_replace(line, SEPARATOR_CHAR, ' ');
 	str_replace(line, '\t', ' ');
